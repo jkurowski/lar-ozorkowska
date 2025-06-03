84dae1d631cb4e8106e8413534e301d6d2ffb38e
Jacek Kurowski <jacek@4dl.pl>
2025-06-03 11:48:24 +0200

    Rodo

--- resources/views/front/investment_property/index.blade.php
+++ resources/views/front/investment_property/index.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.page', ['body_class' => 'investments'])
 
-@section('meta_title', $investment->floor->name . ' - ' . $property->name)
+@section('meta_title', $floor->name . ' - ' . $property->name)
 @section('seo_title', $page->meta_title . ' - ' . $investment->floor->name)
 @section('seo_description', $page->meta_description)
 @section('content')
